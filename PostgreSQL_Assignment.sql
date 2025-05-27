26f148ed801ab73eee8d37122d94a1954cca4048
farhanarnob44 <farhanarnob44@gmail.com>
2025-05-27 21:04:20 +0600

    table creation is done

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,2 +1,61 @@
 SELECT current_database();
 
+--Rangers table
+CREATE Table rangers (
+    ranger_id SERIAL PRIMARY KEY,
+    name VARCHAR(30),
+    region VARCHAR(50)
+);
+
+INSERT INTO rangers (ranger_id, name, region) VALUES 
+    (1, 'Alice Green', 'Northern Hills'),
+    (2, 'Bob White', 'River Delta'),
+    (3, 'Carol King', 'Mountain Range');
+
+--species table
+
+CREATE TABLE species (
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(30),
+    scientific_name VARCHAR(50),
+    discovery_date DATE,
+    conservation_status VARCHAR(50)
+);
+
+INSERT INTO species (species_id, common_name, scientific_name, discovery_date, conservation_status) VALUES
+(1, 'Snow Leopard', 'Panthera uncia', '1775-01-01', 'Endangered'),
+(2, 'Bengal Tiger', 'Panthera tigris tigris', '1758-01-01', 'Endangered'),
+(3, 'Red Panda', 'Ailurus fulgens', '1825-01-01', 'Vulnerable'),
+(4, 'Asiatic Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
+
+
+-- sightings table
+
+CREATE TABLE sightings (
+    sighting_id SERIAL PRIMARY KEY,
+    species_id INT NOT NULL,
+    ranger_id INT NOT NULL,
+    location VARCHAR(200),
+    sighting_time TIMESTAMP,
+    notes VARCHAR(100),
+    FOREIGN KEY (species_id) REFERENCES species(species_id),
+    FOREIGN KEY (ranger_id) REFERENCES rangers(ranger_id)
+);
+
+INSERT INTO sightings (species_id, ranger_id, location, sighting_time, notes) VALUES 
+(1, 1, 'Peak Ridge', '2024-05-10 07:45:00', 'Camera trap image captured'),
+(2, 2, 'Bankwood Area', '2024-05-12 16:20:00', 'Juvenile seen'),
+(3, 3, 'Bamboo Grove East', '2024-05-15 09:10:00', 'Feeding observed'),
+(1, 2, 'Snowfall Pass', '2024-05-18 18:30:00', NULL);
+
+
+
+SELECT * from rangers;
+SELECT * from species;
+SELECT * from sightings;
+
+
+-- table creation is done here
+
+-- *** problem 1 ***
+
